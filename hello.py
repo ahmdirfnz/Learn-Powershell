9f3b45d6be3faacddb7fa62dd39fd17f8928b3bb
Ahmad Irfan Harman (DE-IS/DIGITAL) <ahmadirfan.harman@PETRONAS.COM>
2023-04-04 09:47:05 +0800

    update changes

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import pandas as pd
+import json
+
+# read Excel file into a pandas DataFrame
+df = pd.read_excel('excel playground.xlsx')
+
+# convert DataFrame to JSON
+json_str = df.to_json(orient='records')
+
+# print the JSON string
+print(json_str)
